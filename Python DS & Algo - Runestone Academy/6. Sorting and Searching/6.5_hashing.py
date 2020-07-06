0f722120dfe45bb88320a993147e1c92f28b1291
Faaez Razeen <razeenfaaez@gmail.com>
2020-07-06 14:50:56 +0530

    Added hashmap

---
+++ Python DS & Algo - Runestone Academy/6. Sorting and Searching/6.5_hashing.py
@@ -0,0 +1,52 @@
+class HashMap:
+    def __init__(self, size=11):
+        self.size = size
+        self.slots = [None] * self.size  # Contains the keys
+        self.items = [None] * self.size  # Contains the values
+
+    def hash(self, key):
+        return key % self.size
+
+    def rehash(self, old_hash, skip=1):
+        return (old_hash + skip) % self.size
+
+    def put(self, key, value):
+        if key < 0:
+            key = self.size + key
+        hash = self.hash(key)
+        while self.slots[hash] is not None and self.slots[hash] != key:
+            hash = self.rehash(hash)
+        if self.slots[hash] == key:
+            self.items[hash] = value
+        elif self.slots[hash] is None:
+            self.slots[hash] = key
+            self.items[hash] = value
+
+    def get(self, key):
+        starting_hash = hash = self.hash(key)
+        while self.slots[hash] != key:
+            if self.slots[hash] is None or hash == starting_hash:
+                return None
+            hash = self.hash(hash)
+        return self.items[hash]
+
+    def __setitem__(self, key, value):
+        self.put(key, value)
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+
+H = HashMap()
+H[-1] = "cattus maximus"
+H[26] = "dog"
+H[93] = "lion"
+H[17] = "tiger"
+H[77] = "bird"
+H[31] = "cow"
+H[44] = "goat"
+H[55] = "pig"
+H[20] = "chicken"
+H[20] = 'duck'
+print(H.slots)
+print(H.items)
