8c430719bc1ef5b32f5366828af687ea65882acc
Faaez Razeen <razeenfaaez@gmail.com>
2020-07-07 15:28:09 +0530

    Added comment

--- Python DS & Algo - Runestone Academy/6. Sorting and Searching/C++/6.9_insertion_sort.cpp
+++ Python DS & Algo - Runestone Academy/6. Sorting and Searching/C++/6.9_insertion_sort.cpp
@@ -6,6 +6,7 @@ void insertion_sort(int * arr, int size){
     for(int i = 1; i < size; i++){
         current = i;
         value = arr[i];
+        // when current == 0, there will be no more elements to shift
         while(current > 0 && arr[current-1] > value){
             arr[current] = arr[current-1];
             current = current - 1;
