54fc1d4d79c02e834dab631c8d4ef77dd0539496
Faaez Razeen <razeenfaaez@gmail.com>
2020-07-09 12:26:20 +0530

    Added quicksort with randomized pivot

---
+++ Python DS & Algo - Runestone Academy/6. Sorting and Searching/quick_sort_randomized.py
@@ -0,0 +1,56 @@
+import random
+import timeit
+
+
+def quick_sort(arr, start, end):
+    if start < end:
+        pivot = end-1
+        value = arr[pivot]
+        p_index = start
+        for i in range(start, end):
+            if arr[i] < value:
+                arr[p_index], arr[i] = arr[i], arr[p_index]
+                p_index += 1
+        arr[p_index], arr[pivot] = arr[pivot], arr[p_index]
+        quick_sort(arr, start, p_index)
+        quick_sort(arr, p_index + 1, end)
+
+
+def quick_sort_randomized(arr, start, end):
+    if start < end:
+        rand_pivot = random.randint(start, end-1)
+        arr[rand_pivot], arr[end-1] = arr[end-1], arr[rand_pivot]
+        value = arr[end-1]
+        p_index = start
+        for i in range(start, end):
+            if arr[i] < value:
+                arr[p_index], arr[i] = arr[i], arr[p_index]
+                p_index += 1
+        arr[p_index], arr[end-1] = arr[end-1], arr[p_index]
+        quick_sort(arr, start, p_index)
+        quick_sort(arr, p_index + 1, end)
+
+
+def quick_sort_time(size=1000):
+    for i in range(size):
+        arr = [random.randint(1, 100) for i in range(size)]
+        quick_sort(arr, 0, size)
+
+
+def quick_sort_randomized_time(size=1000):
+    for i in range(size):
+        arr = [random.randint(1, 100) for i in range(size)]
+        quick_sort_randomized(arr, 0, size)
+
+
+t1 = timeit.Timer('quick_sort_time(size)',
+                  'from __main__ import quick_sort_time, size')
+t2 = timeit.Timer('quick_sort_randomized_time(size)',
+                  'from __main__ import quick_sort_randomized_time, size')
+
+
+size = 10
+n_tests = 10000
+dp = 6
+print('Quick Sort:\t\t', round(t1.timeit(n_tests), dp))
+print('Quick Sort Randomized:\t', round(t2.timeit(n_tests), dp))
