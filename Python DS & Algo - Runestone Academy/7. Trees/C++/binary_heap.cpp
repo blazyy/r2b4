d00f27666e11734e7d08d36d71e4a8db6d206909
Faaez Razeen <razeenfaaez@gmail.com>
2020-07-17 18:05:51 +0530

    Added binary heap in C++

---
+++ Python DS & Algo - Runestone Academy/7. Trees/C++/binary_heap.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class BinaryHeap{
+private:
+    vector <int> heap;
+    int size = 0;
+public:
+    BinaryHeap(){
+        heap.push_back(0);
+    }
+    void insert(int value){
+        heap.push_back(value);
+        int idx = ++size;
+        while(heap[idx] < heap[idx/2] && idx > 0){
+            swap(heap[idx], heap[idx/2]);
+            idx /= 2;
+        }
+    }
+    int del_min(){
+        int l_idx, r_idx, idx = 1, min_child_idx, return_val;
+        return_val = heap[1];
+        heap[1] = heap[size--];
+        heap.pop_back();
+        while(idx * 2 <= size){
+            // finding smaller child
+            l_idx = idx * 2;
+            r_idx = idx * 2 + 1;
+            if(r_idx > size)
+                min_child_idx = idx*2;
+            else
+                (heap[l_idx] < heap[r_idx]) ? min_child_idx = l_idx : min_child_idx = r_idx;
+            // If parent is bigger, swap with smaller child of the two childs
+            if(heap[idx] > heap[min_child_idx]){
+                swap(heap[idx], heap[min_child_idx]);
+                idx = min_child_idx;
+            }
+            else break;
+        }
+        return return_val;
+    }
+};
+
+int main(void){
+    BinaryHeap bh;
+}
