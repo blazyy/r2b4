8652c8ca3a0d41874e00ba98da46ef6e84a1df46
Faaez Razeen <razeenfaaez@gmail.com>
2020-07-21 23:43:52 +0530

    Added find_successor() and other helper functions

--- Python DS & Algo - Runestone Academy/7. Trees/binary_search_tree.py
+++ Python DS & Algo - Runestone Academy/7. Trees/binary_search_tree.py
@@ -6,20 +6,27 @@ class Node:
         self.left = left
         self.right = right
 
+    def is_left_child(self):
+        if self.parent is not None and self == self.parent.left:
+            return True
+        return False
+
+    def is_right_child(self):
+        if self.parent is not None and self == self.parent.right:
+            return True
+        return False
+
 
 class BinarySearchTree:
     def __init__(self):
         self.root = None
-        self.size = 0
-
-    def __len__(self):
-        return self.size
 
     def __setitem__(self, key, value):
+        '''I wanted to make this function recursive but couldn't since this
+         is a magic function which means I can't add extra parameters'''
         # If tree is empty
         if self.root is None:
             self.root = Node(key, value)
-            self.size += 1
         else:
             current = self.root
             while current:
@@ -29,31 +36,26 @@ class BinarySearchTree:
                 elif key < current.key:
                     if current.left is None:
                         current.left = Node(key, value, parent=current)
-                        self.size += 1
                         return
                     else:
                         current = current.left
                 else:
                     if current.right is None:
                         current.right = Node(key, value, parent=current)
-                        self.size += 1
                         return
                     else:
                         current = current.right
 
     def __getitem__(self, key):
-        if self.root is None:
-            raise KeyError(key)
-        else:
-            current = self.root
-            while current:
-                if key == current.key:
-                    return current.value
-                elif key < current.key:
-                    current = current.left
-                else:
-                    current = current.right
-            raise KeyError(key)
+        current = self.root
+        while current:
+            if key == current.key:
+                return current.value
+            elif key < current.key:
+                current = current.left
+            else:
+                current = current.right
+        raise KeyError(key)
 
     def __contains__(self, key):
         try:
@@ -62,23 +64,57 @@ class BinarySearchTree:
         except KeyError:
             return False
 
+    def has_no_children(self, node):
+        if node.right is None and node.left is None:
+            return True
+        return False
+
+    def has_one_child(self, node):
+        if node.right is None and node.left is not None:
+            return True
+        elif node.right is not None and node.left is None:
+            return True
+        return False
+
+    def find_min(self, node):
+        while node.left:
+            node = node.left
+        return node.key
+
+    def find_successor(self, node):
+        # If right subtree of node is not null, succ is the smallest element
+        # in the right subtree
+        if node.right is not None:
+            return self.find_min(self.right)
+        else:
+            # If node has no right child and is the left child of its parent,
+            # then the parent is the successor.
+            if node.is_left_child():
+                return node.parent
+            # If node has no right child and is is the right child of its parent
+            # then the successor to this node is the successor of its parent
+            # excluding this node.
+            elif node.is_right_child():
+                self.parent.right = None  # Temporarily excluding the node
+                return self.find_successor(node.parent)
+                self.parent.right = self
+        return None
+
     def delete(self, key):
         # If node to delete is root and there's only one node in the tree
-        if self.size == 1 and key == self.root.key:
+        if key == self.root.key and self.has_no_children(self.root):
             self.root = None
         else:
-            if self.__contains__(key):
+            if self.__contains__(self.root, key):
                 current = self.root
                 while current:
                     if key == current.key:
-                        # If node to delete has no children
-                        if current.left is None and current.right is None:
+                        if self.has_no_children(current):
                             if current.parent.left == current:
                                 current.parent.left = None
                             else:
                                 current.parent.right = None
-                        # If node to delete has one child
-                        elif (current.left is None and current.right is not None) or (current.left is not None and current.right is None):
+                        elif self.has_one_child(current):
                             if current.left is not None:
                                 current.left.parent = current.parent
                                 current.parent.left = current.left
@@ -87,8 +123,7 @@ class BinarySearchTree:
                                 current.parent.right = current.right
                         # If node to delete has two children
                         else:
-                            successor = current.find_successor()
-
+                            successor = self.find_successor(current)
                         self.size -= 1
                         return
                     elif key < current.key:
@@ -102,4 +137,3 @@ bst = BinarySearchTree()
 bst[45] = 'yolo'
 print(45 in bst)
 print(66 in bst)
-print(len(bst))
