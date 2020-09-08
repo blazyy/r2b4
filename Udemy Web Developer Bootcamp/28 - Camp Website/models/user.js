c4d0a5ebfd56d715e32e860866372e9f6c041d81
Faaez Razeen <razeenfaaez@gmail.com>
2020-09-08 13:47:55 +0530

    Added 'about' field in mongo schema

--- Udemy Web Developer Bootcamp/28 - Camp Website/models/user.js
+++ Udemy Web Developer Bootcamp/28 - Camp Website/models/user.js
@@ -3,7 +3,9 @@ const passport_local_mongoose = require('passport-local-mongoose');
 
 const user_schema = mongoose.Schema({
     username: {type: String, unique: true},
+    display_picture: {type: String, default: 'https://www.clipartkey.com/mpngs/m/152-1520367_user-profile-default-image-png-clipart-png-download.png'},
     email: {type: String, unique: true},
+    about: {type: String, default: ''},
     password: String,
     reset_pw_token: String,
     reset_pw_token_expires_on: Date,
