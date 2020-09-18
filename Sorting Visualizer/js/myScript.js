3f955895c24f4b4ae5b5a7d09ff8508370b0b61f
Faaez Razeen <razeenfaaez@gmail.com>
2020-09-18 18:45:25 +0530

    Changed library to p5js

--- Sorting Visualizer/js/myScript.js
+++ Sorting Visualizer/js/myScript.js
@@ -1,70 +1,75 @@
-// Note to self: Paperscript doesn't support ES6, so I can't use let/const and other ES6 stuff.
-var margin_top_percentage = 0.1;
-var window_height = ($(window).height() - ($(window).height() * margin_top_percentage));
-var window_width = $(window).width();
-var num_bars = 100;
+var margin_top_percentage = 0.2;
+var num_bars = 200;
 var bar_color = '#ffffff';
-var increment = window_width / num_bars;
+var bar_width;
 var bar_heights = [];
 var initial_resize = true;
 
-function clear_screen(){
-    project.activeLayer.removeChildren(); // Removes drawn bars
+$(document).ready(function() {
+    const sort_button = document.getElementById('sort-button');
+    // const reset_button = document.getElementById('reset-button');
+    sort_button.addEventListener('click', function(){
+        if(!isLooping()){
+            loop();
+        }
+        bubble_sort();
+    });
+    // reset_button.addEventListener('click', function(){
+    //     noLoop();
+    //     generate_heights();
+    //     resizeCanvas(windowWidth, windowHeight);
+    // });
+});
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    bar_width = windowWidth / num_bars;
+    generate_heights();
 }
 
-function generate_heights(){
+// draw() by default loops forever
+function draw() {
+    background(0);
+    draw_bars();
+    // noLoop();
+}
+
+function generate_heights() {
     bar_heights = [];
-    for(var i = 0; i < num_bars; i++){
-        bar_heights.push(Math.floor(Math.random() * window_height + 1));
+    for (var i = 0; i < num_bars; i++) {
+        bar_heights.push(Math.floor(Math.random() * ((windowHeight) - (windowHeight * margin_top_percentage)) + 1));
     }
 }
 
-function draw_bars(){
+function draw_bars() {
     width_pen = 0;
-    for(var i = 0; i < num_bars; i++){
-        bottom_left = new Point(width_pen, bar_heights[i]);
-        width_pen += increment;
-        top_right = new Point(width_pen, 0);
-        rect = new Rectangle(bottom_left, top_right);
-        path = new Path.Rectangle(rect);
-        path.fillColor = bar_color;
-        path.strokeColor = bar_color
+    for (var i = 0; i < num_bars; i++) {
+        noStroke();
+        fill(255);
+        rect(width_pen, 0, bar_width, bar_heights[i]);
+        width_pen += bar_width;
     }
 }
 
-function bubble_sort(){
-    for(var i = 0; i < num_bars; i++){
-        for(var j = 0; j < num_bars - i - 1; j++){
-            if(bar_heights[j] > bar_heights[j+1]){
+async function bubble_sort() {
+    for (var i = 0; i < num_bars; i++) {
+        for (var j = 0; j < num_bars - i - 1; j++) {
+            if (bar_heights[j] > bar_heights[j + 1]) {
+                await sleep(1);
                 var temp = bar_heights[j];
-                bar_heights[j] = bar_heights[j+1];
-                bar_heights[j+1] = temp;
+                bar_heights[j] = bar_heights[j + 1];
+                bar_heights[j + 1] = temp;
             }
         }
     }
 }
 
-function onResize(event) {
-    // Need this flag because onResize was getting called when the page initially loaded, which I don't want.
-    if(!initial_resize){
-        console.log("RESIZE");
-        initial_resize = false;
-        window_height = ($(window).height() - ($(window).height() * margin_top_percentage));
-        window_width = $(window).width();
-        increment = window_width / num_bars
-        clear_screen(); // Removes drawn bars
-        generate_heights();
-        draw_bars();
-    }
+function windowResized() {
+    bar_width = windowWidth / num_bars;
+    generate_heights();
+    resizeCanvas(windowWidth, windowHeight);
 }
 
-generate_heights();
-draw_bars();
-console.log('before: ', bar_heights);
-// Javascript sorts alphabetically by default. To get it to sort numbers, I have to do this weird trick which doesn't make any sense.
-// bar_heights.sort(function(a, b){
-//     return a-b;
-// });
-bubble_sort();
-draw_bars();
-console.log('after: ', bar_heights);
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
