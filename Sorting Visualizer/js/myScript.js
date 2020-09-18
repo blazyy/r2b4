881e93079c776a022db15cd3f7d2f6290063d7da
Faaez Razeen <razeenfaaez@gmail.com>
2020-09-19 00:54:34 +0530

    Added buttons + other stuff

--- Sorting Visualizer/js/myScript.js
+++ Sorting Visualizer/js/myScript.js
@@ -1,73 +1,77 @@
 var margin_top_percentage = 0.2;
 var num_bars = 200;
-var bar_color = '#ffffff';
+var bar_color = 255;
 var bar_width;
 var bar_heights = [];
 var initial_resize = true;
+var currently_sorting = false;
+var stopped = false;
 
 $(document).ready(function() {
-    const sort_button = document.getElementById('sort-button');
-    // const reset_button = document.getElementById('reset-button');
-    sort_button.addEventListener('click', function(){
-        if(!isLooping()){
-            loop();
-        }
+    $('#start-button').on('click', function(){
+        $('#start-button').attr('disabled', true);
+        $('#reset-button').attr('disabled', true);
         bubble_sort();
     });
-    // reset_button.addEventListener('click', function(){
-    //     noLoop();
-    //     generate_heights();
-    //     resizeCanvas(windowWidth, windowHeight);
-    // });
+
+    $('#reset-button').on('click', function(){
+        $('#start-button').removeAttr('disabled');
+        generate_heights();
+    })
 });
 
 function setup() {
+    frameRate(60);
     createCanvas(windowWidth, windowHeight);
     bar_width = windowWidth / num_bars;
     generate_heights();
 }
 
-// draw() by default loops forever
 function draw() {
     background(0);
     draw_bars();
-    // noLoop();
 }
 
 function generate_heights() {
     bar_heights = [];
     for (var i = 0; i < num_bars; i++) {
-        bar_heights.push(Math.floor(Math.random() * ((windowHeight) - (windowHeight * margin_top_percentage)) + 1));
+        bar_heights.push(Math.floor(Math.random() * ((windowHeight) - (windowHeight * margin_top_percentage))) + 30);
     }
 }
 
 function draw_bars() {
     width_pen = 0;
     for (var i = 0; i < num_bars; i++) {
-        noStroke();
-        fill(255);
-        rect(width_pen, 0, bar_width, bar_heights[i]);
+        stroke(bar_color);
+        fill(bar_color);
+        rect(width_pen, height - bar_heights[i], bar_width, bar_heights[i]);
         width_pen += bar_width;
     }
 }
 
 async function bubble_sort() {
+    currently_sorting = true;
     for (var i = 0; i < num_bars; i++) {
         for (var j = 0; j < num_bars - i - 1; j++) {
             if (bar_heights[j] > bar_heights[j + 1]) {
-                await sleep(1);
+                await sleep(4); // 4 ms is the smallest delay possible using setTimeout()
                 var temp = bar_heights[j];
                 bar_heights[j] = bar_heights[j + 1];
                 bar_heights[j + 1] = temp;
             }
         }
+        // await sleep(1);
     }
+    $('#reset-button').removeAttr('disabled');
+    currently_sorting = false;
 }
 
 function windowResized() {
-    bar_width = windowWidth / num_bars;
-    generate_heights();
-    resizeCanvas(windowWidth, windowHeight);
+    if(!currently_sorting){
+        bar_width = windowWidth / num_bars;
+        generate_heights();
+        resizeCanvas(windowWidth, windowHeight);
+    }
 }
 
 function sleep(ms) {
