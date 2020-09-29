54c87a05b5bd52cc6b04ee5ec3652459f3c766ff
Faaez Razeen <razeenfaaez@gmail.com>
2020-09-29 15:37:35 +0530

    Cleaned up 4.27.x_stack_using_2_queues.py

--- Python DS & Algo - Runestone Academy/4. Basic Data Structures/4.27.x_stack_using_2_queues.py
+++ Python DS & Algo - Runestone Academy/4. Basic Data Structures/4.27.x_stack_using_2_queues.py
@@ -1,33 +1,22 @@
 from queue import Queue
 
+
 class Stack:
     def __init__(self):
-        self.queue_a = Queue()
-        self.queue_b = Queue()
+        self.queueA = Queue()
+        self.queueB = Queue()
 
     def push(self, item):
-        if self.queue_a.isEmpty():
-            self.queue_a.enqueue(item)
-            while not self.queue_b.isEmpty():
-                self.queue_a.enqueue(self.queue_b.dequeue())
-        elif self.queue_b.isEmpty():
-            self.queue_b.enqueue(item)
-            while not self.queue_a.isEmpty():
-                self.queue_b.enqueue(self.queue_a.dequeue())
+        if self.queueA.isEmpty():
+            self.queueA.enqueue(item)
+            while not self.queueB.isEmpty():
+                self.queueA.enqueue(self.queueB.dequeue())
+        else:
+            self.queueB.enqueue(item)
+            while not self.queueA.isEmpty():
+                self.queueB.enqueue(self.queueA.dequeue())
 
     def pop(self):
-        if self.queue_a.isEmpty():
-            return self.queue_b.dequeue()
-        return self.queue_a.dequeue()
-
-stack = Stack()
-stack.push(1)
-stack.push(2)
-stack.push(3)
-stack.pop()
-stack.pop()
-stack.push(4)
-stack.push(5)
-print(stack.pop())
-print(stack.pop())
-print(stack.pop())
+        if self.queueA.isEmpty():
+            return self.queueB.dequeue()
+        return self.queueA.dequeue()
