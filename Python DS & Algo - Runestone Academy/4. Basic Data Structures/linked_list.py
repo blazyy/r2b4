0c9cb5d7735e572d4b51f06fe109a4fb81ceaca9
Faaez Razeen <razeenfaaez@gmail.com>
2020-09-29 18:00:35 +0530

    Revamped linked list

--- Python DS & Algo - Runestone Academy/4. Basic Data Structures/linked_list.py
+++ Python DS & Algo - Runestone Academy/4. Basic Data Structures/linked_list.py
@@ -3,18 +3,6 @@ class Node:
         self.data = data
         self.next = None
 
-    def get_data(self):
-        return self.data
-
-    def get_next(self):
-        return self.next
-
-    def set_data(self, data):
-        self.data = data
-
-    def set_next(self, next):
-        self.next = next
-
 
 class LinkedList:
     def __init__(self):
@@ -23,131 +11,143 @@ class LinkedList:
         self.end = None
 
     def is_empty(self):
-        return self.head is None
-
-    def append_using_pointer(self, item):
-        if self.is_empty():
-            self.head = Node(item)
-            self.end = self.head
-        else:
-            self.end.set_next(Node(item))
-            self.end = self.end.get_next()
+        return self.size == 0
 
     def append(self, item):
-        if self.is_empty():
+        if self.head is None:
             self.head = Node(item)
         else:
-            temp = self.head
-            while temp.get_next() is not None:
-                temp = temp.get_next()
-            temp.set_next(Node(item))
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = Node(item)
         self.size += 1
 
     def index(self, ele):
         idx = 0
         current = self.head
         while current:
-            if current.get_data() == ele:
+            if current.data == ele:
                 return idx
-            current = current.get_next()
+            current = current.next
             idx += 1
         return -1
 
     def pop(self, index_to_pop=-1):
-        current = self.head
+        if self.head is None:
+            raise Exception('Cannot pop, list is empty!')
+        if index_to_pop > self.size - 1:
+            raise IndexError('That index does not exist!')
         if index_to_pop == -1:
-            while current.get_next().get_next():
-                current = current.get_next()
-            return_num = current.get_next().get_data()
-            current.set_next(None)
-        elif index_to_pop == 0:
-            return_num = current.get_data()
-            self.head = current.get_next()
+            index_to_pop = self.size - 1
+        if index_to_pop == 0:
+            return_ele = self.head.data
+            self.head = self.head.next
         else:
-            previous = self.head
             idx = 0
+            current = self.head
+            prev = current
             while current:
                 if idx == index_to_pop:
-                    return_num = current.get_data()
-                    previous.set_next(current.get_next())
-                idx += 1
-                previous = current
-                current = current.get_next()
-        self.size -= 1
-        return return_num
-
-    def insert(self, item, index_to_insert):
-        idx = 0
-        current = self.head
-        prev = self.head
-        if index_to_insert == 0:
-            newNode = Node(item)
-            newNode.set_next(self.head)
-            self.head = newNode
-        else:
-            while current:
-                if idx == index_to_insert:
-                    newNode = Node(item)
-                    prev.set_next(newNode)
-                    newNode.set_next(current)
-                prev = current
-                current = current.get_next()
-                idx += 1
-
-    def remove(self, ele):
-        current = prev = self.head
-        # if element to remove is head
-        if current.get_data() == ele:
-            self.head = current.get_next()
-        else:
-            while current is not None:
-                if current.get_data() == ele:
-                    prev.set_next(current.get_next())
+                    return_ele = current.data
+                    prev.next = current.next
                     break
                 prev = current
-                current = current.get_next()
+                current = current.next
+                idx += 1
         self.size -= 1
+        return return_ele
+
+    def insert(self, item, index_to_insert):
+        if index_to_insert > self.size - 1:
+            raise IndexError('Index cannot be greater than size!')
+        new_node = Node(item)
+        if index_to_insert == 0:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            prev = current
+            idx = 0
+            while current:
+                if idx == index_to_insert:
+                    new_node.next = current
+                    prev.next = new_node
+                    break
+                prev = current
+                current = current.next
+                idx += 1
+        self.size += 1
+
+    def remove(self, ele):
+        if self.head is None:
+            raise Exception('Cannot remove, list is empty!')
+        if self.head.data == ele:
+            self.head = self.head.next
+            self.size -= 1
+            return
+        else:
+            current = self.head
+            prev = current
+            while current:
+                if current.data == ele:
+                    prev.next = current.next
+                    self.size -= 1
+                    return
+                prev = current
+                current = current.next
+            raise Exception('Element not found!')
+
 
     def search(self, ele):
-        current = self.head
-        idx = 0
-        while current is not None:
-            if current.get_data() == ele:
-                return 'True: {}'.format(idx)
-            idx += 1
-            current = current.get_next()
+        if self.head is None:
+            raise Exception('Cannot search if the list is empty!')
+        if self.head.data == ele:
+            return True
+        current = self.head.next
+        while current:
+            if current.data == ele:
+                return True
+            current = current.next
         return False
 
     def slice(self, start_idx, end_idx):
-        lst = []
+        if start_idx < 0 or end_idx > self.size - 1:
+            raise IndexError('Invalid indexes!')
         current = self.head
         idx = 0
+        sliced = []
         while current:
-            if idx >= start_idx and idx < end_idx:
-                lst.append(current.get_data())
-            if idx >= end_idx:
-                break
-            current = current.get_next()
+            if idx == start_idx:
+                sliced = []
+                if start_idx == end_idx:
+                    return sliced
+                while idx != end_idx:
+                    sliced.append(current.data)
+                    current = current.next
+                    idx += 1
+                return sliced
+            current = current.next
             idx += 1
-        return '[' + ', '.join(str(val) for val in lst) + ']'
 
     def get_size(self):
         return self.size
 
     def get_index(self, ele):
-        current = self.head
-        idx = 0
+        if self.head.data == ele:
+            return 0
+        current = self.head.next
+        idx = 1
         while current:
-            if current.get_data() == ele:
+            if current.data == ele:
                 return idx
+            current = current.next
             idx += 1
-            current = current.get_next()
-        return -1
 
     def __str__(self):
         current = self.head
-        lst = []
-        while current is not None:
-            lst.append(current.get_data())
-            current = current.get_next()
-        return '[' + ', '.join(str(val) for val in lst) + ']'
+        elements = []
+        while current:
+            elements.append(current.data)
+            current = current.next
+        return ' '.join([str(element) for element in elements])
