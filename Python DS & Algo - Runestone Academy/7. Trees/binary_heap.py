66fe2eaa6951c7721b0d82843ee2c9a156774175
Faaez Razeen <razeenfaaez@gmail.com>
2020-10-03 16:38:48 +0530

    Rewrote binary heap

--- Python DS & Algo - Runestone Academy/7. Trees/binary_heap.py
+++ Python DS & Algo - Runestone Academy/7. Trees/binary_heap.py
@@ -1,73 +1,62 @@
 import random
 
-
 class BinaryHeap:
-    def __init__(self, heap=[]):
-        self.heap = [0] + heap  # 0 to make integer divison possible
-        self.size = len(heap)
-        if heap != []:
-            self.build_heap()
+    def __init__(self):
+        self.heap = [0]
+        self.size = 0
 
-    def is_empty(self):
-        return self.size == 0
-
-    def get_size(self):
-        return self.size
-
-    def insert(self, value):
-        self.heap.append(value)
+    def insert(self, item):
+        self.heap.append(item)
         self.size += 1
-        idx = self.size  # no -1 since the first ele of heap list isn't used.
-        while self.heap[idx] < self.heap[idx//2] and idx > 0:
-            self.heap[idx], self.heap[idx//2] = self.heap[idx//2], self.heap[idx]
-            idx = idx // 2
+        self.perc_up(self.size)
 
-    def build_heap(self):
-        '''
-        Why do we start at the middle and move to the root?
-        Although we start out in the middle of the tree and work our way back
-        toward the root, the perc_down method ensures that the largest child is
-        always moved down the tree. Because the heap is a complete binary tree,
-        any nodes past the halfway point will be leaves and therefore have no
-        children.
-        '''
-        for i in range(self.size//2, 0, -1):
-            self.perc_down(i)
 
-    def find_min(self):
-        return self.heap[1]
-
-    def perc_down(self, idx):
-        while idx * 2 <= self.size:
-            # Finding min child
-            l_idx = idx * 2
-            r_idx = idx * 2 + 1
-            if r_idx > self.size or self.heap[l_idx] < self.heap[r_idx]:
-                min_child_idx = l_idx
-            else:
-                min_child_idx = r_idx
-            # Swapping current root with smaller child
-            if self.heap[idx] > self.heap[min_child_idx]:
-                self.heap[idx], self.heap[min_child_idx] = self.heap[min_child_idx], self.heap[idx]
-                idx = min_child_idx
-            else:
-                break
+    def perc_up(self, idx):
+        while idx // 2 > 0:
+            if self.heap[idx] < self.heap[idx // 2]:
+                self.heap[idx], self.heap[idx // 2] = self.heap[idx//2], self.heap[idx]
+            idx //= 2
 
     def del_min(self):
         return_val = self.heap[1]
-        self.heap[1] = self.heap[self.size]
+        self.heap[1] = self.heap[self.size] # replacing root node with last node in heap
         self.heap.pop()
         self.size -= 1
-        self.perc_down(idx=1)
+        self.perc_down(1)
         return return_val
+
+    def perc_down(self, idx):
+        while idx * 2 <= self.size:
+            min_child_idx = self.get_min_child_idx(idx)
+            if self.heap[idx] > self.heap[min_child_idx]:
+                self.heap[idx], self.heap[min_child_idx] = self.heap[min_child_idx], self.heap[idx]
+            idx = min_child_idx
+
+    def get_min_child_idx(self, idx):
+        if (idx * 2 + 1) > self.size:
+            # If there is no right child, return left child's index
+            return idx * 2
+        else:
+            if self.heap[idx * 2] < self.heap[idx * 2 + 1]:
+                return idx * 2
+            else:
+                return idx * 2 + 1
+
+    def build_heap(self, lst):
+        self.size = len(lst)
+        self.heap = [0] + lst[:]
+        idx = len(lst) // 2
+        while idx > 0:
+            self.perc_down(idx)
+            idx -= 1
 
     def __str__(self):
         return ' '.join(str(num) for num in self.heap)
 
-
 size = 10
 lst = [random.randint(1, 100) for i in range(size)]
-bh = BinaryHeap(lst)
+bh = BinaryHeap()
+bh.build_heap(lst)
 print('Random List:\t\t\t', lst)
 print('Sorted List:\t\t\t', sorted(lst))
 print('Binary Heap Deleted Minimums:\t', [bh.del_min() for i in range(size)])
