297af1658de4bdb491f313646fc3f6c82034c8e2
Faaez Razeen <razeenfaaez@gmail.com>
2020-10-30 22:26:15 +0530

    4.4 unfinished

---
+++ CTCI/Interview Questions/Python/4.4_check_balanced.py
@@ -0,0 +1,8 @@
+# Implement a function to check if a binary tree is balanced. For the purposes of this question, a balanced tree is defined to be a tree such that the heights of thr two subtrees of nay node never differ by more than one.
+
+from binarytree import tree
+
+# def is_balanced()
+
+my_tree = tree(height=5, is_perfect=False) # tree() returns the root node of the tree.
+print(my_tree)
