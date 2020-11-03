2ab7f59d91b12dec1501b8125bdf2af140ea29d3
Faaez Razeen <razeenfaaez@gmail.com>
2020-11-03 23:45:14 +0530

    4.7

--- CTCI/Interview Questions/C++/4.7_build_order.cpp
+++ CTCI/Interview Questions/C++/4.7_build_order.cpp
@@ -2,6 +2,81 @@
 
 // Example:
 // Input:
-//       projects : a, b, c, d, e, fail
+//       projects : a, b, c, d, e, f
 //       dependencies: (a, d), (f, b), (b, d), (f, a), (d, c)
 // Output: f, e, a, b, d, c
+
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+class Graph{
+private:
+    unordered_map <char, vector <char>> graph;
+    int num_projects = 0;
+public:
+    Graph(vector <vector <char>> connections, vector <char> projects){
+        for(auto nodes : connections){
+            if(graph.find(nodes[1]) == graph.end())
+                graph[nodes[1]] = vector <char> {};
+            graph[nodes[1]].push_back(nodes[0]);
+        }
+        for(auto project : projects){
+            num_projects++;
+            if(graph.find(project) == graph.end())
+                graph[project] = vector <char> {};
+        }
+    }
+    void remove_node_from_dependencies(char node_to_remove){
+        // for(auto const &node : graph)
+        unordered_map <char, vector <char>>::iterator it;
+        for(it = graph.begin(); it != graph.end(); it++)
+            if(find(it -> second.begin(), it -> second.end(), node_to_remove) != it -> second.end())
+                it -> second.erase(find(it -> second.begin(), it -> second.end(), node_to_remove));
+    }
+    bool has_dependencies(){
+        for(auto const &node : graph)
+            if(graph[node.first].size() > 0) return true;
+        return false;
+    }
+    vector <char> build_order(){
+        vector <char> build_order = {};
+        vector <char> removed_nodes = {};
+        while(removed_nodes.size() < num_projects){
+            for(auto const& node : graph){
+                if(find(removed_nodes.begin(), removed_nodes.end(), node.first) == removed_nodes.end() && node.second.size() == 0){
+                    build_order.push_back(node.first);
+                    remove_node_from_dependencies(node.first);
+                    removed_nodes.push_back(node.first);
+                }
+            }
+        }
+        if(has_dependencies()) return vector <char> {};
+        return build_order;
+    }
+    void print(){
+        for(auto const& node : graph){
+            cout << node.first << ": ";
+            for(auto adjacent : node.second)
+                cout << adjacent << " ";
+            cout << endl;
+        }
+    }
+};
+
+void print_vector(vector <char> vect){
+    for (auto ch : vect) cout << ch << " ";
+    cout << endl;
+}
+
+int main(void){
+    vector <vector <char>> connections = {
+        {'a', 'd'}, {'f', 'b'}, {'b', 'd'}, {'f', 'a'}, {'d', 'c'}
+    };
+    vector <char> projects = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
+    Graph graph(connections, projects);
+    graph.print();
+    print_vector(graph.build_order());
+}
--- CTCI/Interview Questions/Python/4.7_build_order.py
+++ CTCI/Interview Questions/Python/4.7_build_order.py
@@ -5,3 +5,59 @@
 #       projects : a, b, c, d, e, f
 #       dependencies: (a, d), (f, b), (b, d), (f, a), (d, c)
 # Output: f, e, a, b, d, c
+
+import pprint
+
+class Graph:
+    """Directed Graph Class"""
+    def __init__(self, connections, projects):
+        self.graph = {}
+        self.num_projects = 0
+        self.add_connections(connections, projects)
+
+    def add_connections(self, connections, projects):
+        for node1, node2 in connections:
+            if node2 not in self.graph:
+                self.graph[node2] = []
+            self.graph[node2].append(node1)
+        for project in projects:
+            self.num_projects += 1
+            if project not in self.graph:
+                self.graph[project] = []
+
+    def __str__(self):
+        return '{}({})'.format(self.__class__.__name__, dict(self.graph)) # Casting to dict() just to make output cleaner
+
+    def remove_node_from_dependencies(self, node_to_remove):
+        for node in self.graph:
+            if node_to_remove in self.graph[node]:
+                self.graph[node].remove(node_to_remove)
+
+    def has_dependencies(self):
+        for node in self.graph:
+            if len(self.graph[node]) > 0:
+                return True
+        return False
+
+    def build_order(self):
+        build_order = []
+        removed_nodes = []
+        while len(removed_nodes) < self.num_projects:
+            for node in self.graph:
+                if node not in removed_nodes and self.graph[node] == []:
+                    build_order.append(node)
+                    self.remove_node_from_dependencies(node)
+                    removed_nodes.append(node)
+        if self.has_dependencies():
+            return []
+        return build_order
+
+
+connections = [('a', 'd'), ('f', 'b'), ('b', 'd'), ('f', 'a'), ('d', 'c')]
+projects = ['a', 'b', 'c', 'd', 'e', 'f']
+# connections = [('f', 'a'), ('f', 'c'), ('f', 'b'), ('c', 'a'), ('b', 'a'), ('a', 'e'), ('b', 'e'), ('d', 'g')]
+projects = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+graph = Graph(connections, projects)
+pprint.PrettyPrinter().pprint(graph.graph)
+print(graph.build_order())
+# pprint.PrettyPrinter().pprint(graph.graph)
