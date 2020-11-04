35e95ca51a21018a01c16760d19e6d27e4126e9c
Faaez Razeen <razeenfaaez@gmail.com>
2020-11-04 13:53:00 +0530

    Added DFS solution

--- CTCI/Interview Questions/Python/4.7_build_order.py
+++ CTCI/Interview Questions/Python/4.7_build_order.py
@@ -10,16 +10,22 @@ import pprint
 
 class Graph:
     """Directed Graph Class"""
-    def __init__(self, connections, projects):
+    def __init__(self, connections, projects, reverse=False):
         self.graph = {}
         self.num_projects = 0
-        self.add_connections(connections, projects)
+        self.add_connections(connections, projects, reverse)
 
-    def add_connections(self, connections, projects):
-        for node1, node2 in connections:
-            if node2 not in self.graph:
-                self.graph[node2] = []
-            self.graph[node2].append(node1)
+    def add_connections(self, connections, projects, reverse):
+        if reverse: # Used in DFS solution.
+            for node1, node2 in connections:
+                if node1 not in self.graph:
+                    self.graph[node1] = []
+                self.graph[node1].append(node2)
+        else:
+            for node1, node2 in connections:
+                if node2 not in self.graph:
+                    self.graph[node2] = []
+                self.graph[node2].append(node1)
         for project in projects:
             self.num_projects += 1
             if project not in self.graph:
@@ -39,25 +45,67 @@ class Graph:
                 return True
         return False
 
-    def build_order(self):
+    # Solution 1
+    # Use graph notation, and remove nodes which do not have any dependencies, i.e. incoming nodes.
+    # If all nodes have been dealt with and there are dependencies left, return error.
+    # Time Complexity: O(p + d) where p is the no. of projects, d is the no. of dependencies
+    def get_build_order(self):
         build_order = []
-        removed_nodes = []
-        while len(removed_nodes) < self.num_projects:
+        num_removed = 0
+        while num_removed < self.num_projects:
             for node in self.graph:
-                if node not in removed_nodes and self.graph[node] == []:
+                if node not in build_order and self.graph[node] == []:
                     build_order.append(node)
                     self.remove_node_from_dependencies(node)
-                    removed_nodes.append(node)
+                    num_removed += 1
         if self.has_dependencies():
             return []
         return build_order
 
+    def get_unvisited_neighbour(self, node, visited):
+        for neighbour in self.graph[node]:
+            if neighbour not in visited:
+                return neighbour
+        return None
 
-connections = [('a', 'd'), ('f', 'b'), ('b', 'd'), ('f', 'a'), ('d', 'c')]
-projects = ['a', 'b', 'c', 'd', 'e', 'f']
-# connections = [('f', 'a'), ('f', 'c'), ('f', 'b'), ('c', 'a'), ('b', 'a'), ('a', 'e'), ('b', 'e'), ('d', 'g')]
+    def choose_random_unvisited_node(self, visited):
+        for node in self.graph.keys():
+            if node not in visited:
+                return node
+        return None
+
+    # Solution 2
+    # Using DFS. Traverse until a node w/o neighbours or w/o unvisitd neighbours is seen. At the point, we know that it's the last to get added to the build order since no other projects depend on this. So add to build order. We'll reverse the build order at the end.
+    # Time Complexity: O(p + d) where p is the no. of projects, d is the no. of dependencies
+    # Space Complexity: O(p)
+    def get_build_order_dfs(self):
+        # Solution doesn't account for nodes which cannot be built. This problem for whatever reason was so frustrating that I'm annoyed and will not be proceeding with implementing it in C++
+        build_order = []
+        stack = []
+        visited = []
+        node = self.choose_random_unvisited_node(visited)
+        stack.append(node)
+        visited.append(node)
+        while len(stack) > 0:
+            neighbour = self.get_unvisited_neighbour(stack[-1], visited)
+            if neighbour is not None:
+                stack.append(neighbour)
+                visited.append(neighbour)
+            else:
+                build_order.append(stack.pop())
+            if len(stack) == 0:
+                node = self.choose_random_unvisited_node(visited)
+                if node is not None:
+                    stack.append(node)
+                    visited.append(node)
+        return build_order[::-1]
+
+connections = [('f', 'a'), ('f', 'c'), ('f', 'b'), ('c', 'a'), ('b', 'a'), ('a', 'e'), ('b', 'e'), ('d', 'g')] # ['e', 'g', 'a', 'd', 'c', 'b', 'f']
 projects = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-graph = Graph(connections, projects)
+# connections = [('a', 'd'), ('f', 'b'), ('b', 'd'), ('f', 'a'), ('d', 'c')] # ['e', 'f', 'b', 'a', 'd', 'c']
+# projects = ['a', 'b', 'c', 'd', 'e', 'f']
+graph = Graph(connections, projects, reverse=False)
 pprint.PrettyPrinter().pprint(graph.graph)
-print(graph.build_order())
+print(graph.get_build_order())
+# print(graph.get_build_order_dfs())
 # pprint.PrettyPrinter().pprint(graph.graph)
