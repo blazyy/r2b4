21e9e9238bafc00333d9d4d31f18b3f41b664a36
Faaez <razeenfaaez@gmail.com>
2021-01-30 22:52:05 +0530

    Update helper.h

--- LB450/helper.h
+++ LB450/helper.h
@@ -5,6 +5,7 @@
 #define HELPER_H
 
 #include <iostream>
+#include <bits/stdc++.h>
 #include <algorithm>
 #include <vector>
 #include <ctime>
@@ -13,18 +14,48 @@
 
 using namespace std;
 
-vector <int> generate_random_vector(int size=10) {
+vector <int> generate_random_vector(int size=10, int upper_bound=100, bool include_negative=false) {
     vector <int> vect;
-    for(int i = 0; i < size; i++)
-        vect.push_back(rand() % 100);
+    for(int i = 0; i < size; i++) {
+        if(include_negative)
+            vect.push_back((rand() % upper_bound) - (upper_bound / 2));
+        else
+            vect.push_back(rand() % upper_bound);
+    }
     return vect;
 }
+
+vector <int> generate_distinct_random_vector(int size=10, int upper_bound=100) {
+    if(size > upper_bound) {
+        cout << "Upper bound of number generation too small for given size!" << endl;
+        exit(0);
+    }
+    vector <int> vect;
+    int i = 0;
+    while(i < size) {
+        int rand_num = rand() % 100;
+        if(find(vect.begin(), vect.end(), rand_num) != vect.end()) continue;
+        else {
+            vect.push_back(rand_num);
+            i++;
+        }
+    }
+    return vect;
+}
+
+
 
 void print_array(vector <int> &arr, bool new_line=true) {
     if(new_line) cout << endl;
     for(auto num : arr)
         cout << num << " ";
     cout << endl;
+}
+
+void shuffle_array(vector <int> &arr) {
+    unsigned seed = 0;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    shuffle(arr.begin(), arr.end(), default_random_engine(seed));
 }
 
 #endif
