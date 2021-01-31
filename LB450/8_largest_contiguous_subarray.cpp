36a2ee9371aed04219664581b93a5b2fd2c91a01
Faaez <razeenfaaez@gmail.com>
2021-01-31 20:31:48 +0530

    Add Kadane's with index tracking

--- LB450/8_largest_contiguous_subarray.cpp
+++ LB450/8_largest_contiguous_subarray.cpp
@@ -21,17 +21,38 @@ void solution_1(vector <int> &arr) {
     cout << endl << "Maximum sum is " << highest << ", from index " << start << " to " << end << endl;
 }
 
+// Kadane's Algorithm without subarray indices
+// void solution_2(vector <int> &arr) {
+//     int current_sum, max_sum, start, end;
+//     current_sum = max_sum = arr[0];
+//     for(int i = 1; i < arr.size(); i++) {\
+//         current_sum = max(arr[i], arr[i] + current_sum); // If this doesn't make sense, remember that the array contains negative numbers
+//         max_sum = max(max_sum, current_sum);
+//     }
+//     cout << "Maximum sum is " << max_sum << endl;
+// }
+
 void solution_2(vector <int> &arr) {
     // Kadane's Algorithm
     // TC - O(n)
     // SC - O(1)
-    int current_sum, max_sum, start, end;
-    current_sum = max_sum = arr[0];
+    int current_sum = arr[0], max_sum = arr[0], start = 0, end = 0, max_start = 0, max_end = 0;
     for(int i = 1; i < arr.size(); i++) {
-        current_sum = max(arr[i], arr[i] + current_sum); // If this doesn't make sense, remember that the array contains negative numbers
-        max_sum = max(max_sum, current_sum);
+        if(arr[i] > arr[i] + current_sum) {
+            current_sum = arr[i];
+            start = end = i;
+        }
+        else {
+            current_sum = arr[i] + current_sum;
+            end = i;
+        }
+        if(current_sum > max_sum) {
+            max_sum = current_sum;
+            max_start = start;
+            max_end = end;
+        }
     }
-    cout << "Maximum sum is " << max_sum << endl;
+    cout << "Maximum sum is " << max_sum << ", from index " << max_start << " to " << max_end << endl;
 }
 
 int main(void) {
